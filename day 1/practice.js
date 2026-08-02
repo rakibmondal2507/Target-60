d9e29ac074402fdca9a6295ab584866854f1720c
Rakib Mondal <rakibmondal462@gmail.com>
2026-08-02 20:47:07 +0530

    Practice set

---
+++ day 1/practice.js
@@ -0,0 +1,71 @@
+//Addition
+
+// let a= 10;
+// let b= 20;
+
+// const Addition = (a,b) => a+b;
+// const Subtraction = (a,b) => a-b;
+// const Division = (a,b) => a/b;
+// const Multiplication = (a,b) => a*b;
+// const square = (a,b) => a**b;
+
+
+
+
+// console.log(Addition(a,b))
+// console.log(Subtraction(a,b))
+// console.log(Division(a,b))
+// console.log(Multiplication(a,b))
+// console.log(square(a,b))
+
+
+// even/odd
+
+
+// let num = 21
+
+// if(num %2==0){
+//     console.log(`${num} is even`)
+// }else{
+//      console.log(`${num} is odd`)
+// }
+
+//greeting function
+
+// let name = "Payel"
+
+// const greet = (name) => `Hello ${name}, Good evening`
+
+// console.log(greet(name))
+
+
+//Maximum of Two Numbers
+
+// let a = 22
+// let b = 53
+
+// const max_num = (a, b)=> Math.max(a,b)
+
+// const max_num = (a, b) =>{
+//     if(a > b){
+//         return a
+//     }else{
+//         return b
+//     }
+// }
+
+// console.log(max_num(a,b))
+
+//tempreture converter
+
+// let fahrenheit = 104
+
+// const temp_convert = fahrenheit => (fahrenheit - 32) * 5 / 9;
+
+// console.log(temp_convert(fahrenheit))
+
+//Area of Circle
+
+const area = (radius) =>Math.PI * (radius*radius)
+
+console.log(area(5))
